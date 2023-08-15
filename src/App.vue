c62d71e1469116d80f760521f01771b442b44b26
zxk12 <141977192+zxk12@users.noreply.github.com>
2023-08-15 17:11:05 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="https://i01piccdn.sogoucdn.com/bd0691ec4c9a26f1">
   </div>
 </template>
 
